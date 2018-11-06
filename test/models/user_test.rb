c78878b5304a69faea1b7c89ef3618a620c271d0
fairuz <fairuzyassar7@gmail.com>
2018-11-06 10:21:52 +0700

    finish secure password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -50,6 +50,22 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "user should have a password" do
+    user_no_password = User.new(name: "example", email: "example@example.com")
+    user_no_password.save
+    assert_not user_no_password.valid?
+  end
+
+  test "password should be non blank" do
+    @user.password = @user.password_confirmation = " " * 10
+    assert_not @user.valid?
+  end
+
+  test "password should reach minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 end
