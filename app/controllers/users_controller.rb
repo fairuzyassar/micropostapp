0684ff8e38db55130a25a5975611e00d3c1433d7
fairuz <fairuzyassar7@gmail.com>
2018-11-08 12:25:22 +0700

    done signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  		flash[:success] = "Welcome to Micropost"
+  		redirect_to @user
   	else
   		render 'new' 	
   	end
